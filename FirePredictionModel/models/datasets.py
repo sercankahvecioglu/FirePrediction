df00e12b8b44b553aa72d4b549fc41ffcf518014
dariocarretta <darioca02@gmail.com>
2025-08-09 13:56:20 +0300

    Applied changes to the pipelino to allow new data obtainment, integrated clouddetector in pipeline, started changing geospatial data obtainment

--- FirePredictionModel/models/datasets.py
+++ FirePredictionModel/models/datasets.py
@@ -18,7 +18,7 @@ class Sent2Dataset(Dataset):
         - transform (OPTIONAL): transformation to apply to each input image
         - target_transform (OPTIONAL): transformation to apply to each label
     """
-    def __init__(self, input_data_dir, labels_dir, transform=None, target_transform=None):
+    def __init__(self, input_data_dir, labels_dir, geoinfo_path, transform=None, target_transform=None):
         self.input_data_dir = input_data_dir
         self.input_list = glob.glob(os.path.join(input_data_dir, "*.npy"))
         self.labels_dir = labels_dir
@@ -26,27 +26,19 @@ class Sent2Dataset(Dataset):
         self.transform = transform
         self.target_transform = target_transform
         # geospatial info part
-        self.geoinfo_paths = glob.glob(os.path.join(input_data_dir, "*_geoinfo.pkl"))
-        self.bands_info_paths = glob.glob(os.path.join(input_data_dir, "*_band_info.pkl"))
-        self.country_ids = [os.path.basename(path).split('_geoinfo.pkl')[0] for path in self.geoinfo_paths]
+        self.geoinfo_paths = glob.glob(os.path.join(geoinfo_path, "*_tiles_data.pkl"))
+        self.country_ids = [os.path.basename(path).split('_tiles_data.pkl')[0] for path in self.geoinfo_paths]
         # get pickle data extracted
         self.geoinfo = []
         for path in self.geoinfo_paths:
             with open(path, 'rb') as f:
                 self.geoinfo.append(pickle.load(f))
-        with open(self.bands_info_paths[0], "rb") as f:
-            bands_info = pickle.load(f)
-        self.bands_info = bands_info
 
         # retrieve indices for required bands (NIR, SWIR1, SWIR2, NDVI, NDMI)
         required_bands = ['B08', 'B11', 'B12', 'NDVI', 'NDMI']
-        # bands_info is a dict idx:band, so we need to find indices where band matches required_bands
-        self.band_indices = [idx for idx, band in bands_info['band_order'].items() if band in required_bands]
+        # select required indices for above defined bands
+        self.band_indices = [7, 11, 12, 13, 14]
         
-        # Separate indices for reflectance bands vs spectral indices
-        self.reflectance_indices = [i for i, idx in enumerate(self.band_indices) if bands_info['band_order'][idx] in ['B08', 'B11', 'B12']]
-        self.spectral_indices = [i for i, idx in enumerate(self.band_indices) if bands_info['band_order'][idx] in ['NDVI', 'NDMI']]
-
         # create dict with pkl info for each country
         self.geodict = dict(zip(self.country_ids, self.geoinfo))
 
@@ -63,7 +55,7 @@ class Sent2Dataset(Dataset):
         coordy, coordx = map(int, realcoords.replace('.npy', '').strip("()").split(", "))
         # get real-world coords from geoinfo dict
         geoinfo = self.geodict[country_id]
-        coords = get_real_world_coords(coordy, coordx, geoinfo)
+        coords = get_real_world_coords(coordy, coordx, metadata=geoinfo)
         label_path = self.labels_list[index]
         # Load input and select only required bands
         input_tensor = np.load(input_path)
@@ -77,17 +69,17 @@ class Sent2Dataset(Dataset):
         input_tensor = input_tensor.permute(2, 0, 1)
         label_tensor = label_tensor.permute(2, 0, 1)
         
-        # Normalize different band types appropriately
-        # Scale reflectance bands (0-10000) to (0-1)
-        for i in self.reflectance_indices:
-            input_tensor[i] = input_tensor[i] / 10000.0
+        # apply minmax normalization to all bands (separately)
+        for i in range(input_tensor.shape[0]):
+            band = input_tensor[i]
+            min_val = band.min()
+            max_val = band.max()
+            if max_val > min_val:  # avoid division by zero
+                input_tensor[i] = (band - min_val) / (max_val - min_val) 
+            else:
+                input_tensor[i] = torch.zeros_like(band)
         
-        # Clip and scale spectral indices (-1 to 1) to (0-1)
-        for i in self.spectral_indices:
-            input_tensor[i] = torch.clamp(input_tensor[i], -1, 1)
-            input_tensor[i] = (input_tensor[i] + 1) / 2.0
-        
-        # Apply transforms if provided
+        # apply transforms if provided
         if self.transform:
             input_tensor = self.transform(input_tensor)
         if self.target_transform:
@@ -98,3 +90,5 @@ class Sent2Dataset(Dataset):
 
 
 
+if __name__ == '__main__':
+    ds = Sent2Dataset("/home/dario/Desktop/FirePrediction/TILES_INPUT_DATA", "/home/dario/Desktop/FirePrediction/TILES_LABELS", "/home/dario/Desktop/FirePrediction/data_pkl")
--- data_pipeline/pipeline.py
+++ data_pipeline/pipeline.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.bands_preprocessing import *
 from utils.geodata_extraction import extract_geospatial_metadata
+from utils.clouddetector import is_cloudy
+import time
 
 
 
@@ -10,8 +12,7 @@ from utils.geodata_extraction import extract_geospatial_metadata
 def full_sentinel2_data_pipeline(dataset_name: str, 
                                    base_path: str = '/home/dario/Desktop/FirePrediction',
                                    patch_size: tuple = (256, 256),
-                                   threshold: float = 0.6,
-                                   cloud_threshold: float = 0.6):
+                                   cloud_threshold: float = 0.5):
     """
     Complete processing pipeline for satellite data including bands extraction, cloud filtering, 
     vegetation filtering, and NDVI/NDMI extraction.
@@ -37,12 +38,8 @@ def full_sentinel2_data_pipeline(dataset_name: str,
     os.makedirs(tiles_labels_path, exist_ok=True)
     
     # Step 1: Extract data from folder and read all 13 bands
-    print("\n--- Step 1: Reading all 13 bands data ---")
     all_bands = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B10', 'B11', 'B12']
-    # extracting pre bands
-    print("Reading pre-fire bands...")
-    pre_bands = read_sent2_1c_bands(f'{dataset_name}_pre', all_bands)
-    # saving geospatial profile
+    """
     print("Saving geospatial information...")
     extract_geospatial_metadata(dataset_name, os.path.join(base_path, "DATASETS"), tiles_input_path)
     print("Geospatial information saved successfully!\n")
@@ -56,67 +53,45 @@ def full_sentinel2_data_pipeline(dataset_name: str,
         if 'resampling_info' in pre_bands:
             band_info['resampling_info'] = pre_bands['resampling_info']
         pickle.dump(band_info, f)
-
-    # extracting post bands
-    print("Reading post-fire bands...")
-    post_bands = read_sent2_1c_bands(f'{dataset_name}_post', ['B04', 'B08', 'B8A', 'B12'])
-    
-    # Print information about the result
-    print("Band names:", pre_bands['band_names']) 
-    print("Data shape:", pre_bands['data'].shape)
-    print("Band order:", pre_bands['band_order'])
-    if 'resampling_info' in pre_bands:
-        print("Resampling info:", pre_bands['resampling_info'])
-
-    bands_data = pre_bands['data']  # Use pre-fire data for tiles
-    # Get band names for vegetation index computation
-    band_names = pre_bands['band_names']
-
+"""
     #------------------------------------------------------------------------------------
     
-    # Step 2: Create label map (dNBR)
-    print("\n--- Step 2: Creating label map (dNBR) ---")
+    pre_path = os.path.join(base_path, 'data', f'{dataset_name}_pre.npy')
+    post_path = os.path.join(base_path, 'data', f'{dataset_name}_post.npy')
+    # Step 1: Create label map (dNBR)
+    print("\n--- Step 1: Creating label map (dNBR) ---")
     os.makedirs(full_img_path, exist_ok=True)
-    extract_data_labels_from_bands(pre_bands, post_bands, full_img_path, threshold)
+    dnbr_path = extract_data_labels(pre_path, post_path, full_img_path)
     print("✓ dNBR label map created")
 
-    # free up ram space 
-    del post_bands, pre_bands
-
     #------------------------------------------------------------------------------------
     
-    # Step 3: Divide into tiles of size (256, 256, 13)
-    print("\n--- Step 3: Extracting tiles (256, 256, 13) ---")
+    # Step 2: Divide into tiles of size (256, 256, 13)
+    print("\n--- Step 2: Extracting tiles (256, 256, 13) ---")
     num_bands = len(all_bands)
-    extract_tiles_with_padding(bands_data, dataset_name, (*patch_size, num_bands), tiles_input_path)
-    
-    # free up ram
-    del bands_data
+    init_time = time.time()
+    extract_tiles_with_padding(pre_path, dataset_name, (*patch_size, num_bands), tiles_input_path)
+    dt = time.time() - init_time
+    print(f"✓ input tiles extracted to {tiles_input_path} in {dt:.1f} seconds") 
 
     # Also extract label tiles
     dnbr_normmap = np.load(os.path.join(full_img_path, 'dnbr_normalized.npy'))
-    extract_tiles_with_padding(dnbr_normmap, dataset_name, (*patch_size, 1), tiles_labels_path)
-    print(f"✓ tiles extracted to {tiles_input_path}")
+    extract_tiles_with_padding(dnbr_path, dataset_name, (*patch_size, 1), tiles_labels_path)
+    print(f"✓ label tiles extracted to {tiles_input_path}")
 
     #------------------------------------------------------------------------------------
-
-    # TODO: put band info and profile info into the tiles folder
     
-    """
-    # Step 4: Apply cloud detection function
-    print("\n--- Step 4: Applying cloud detection ---")
-    print(f"Using S2PixelCloudDetector with threshold: {cloud_threshold}")
-    cloud_results = patch_cloud_detection(tiles_input_path, cloud_threshold)
-    if cloud_results is not None:
-        visualize_patch_results(cloud_results)
-        print(f"✓ Cloud detection completed. Clean tiles: {cloud_results['clean_tiles']}, Cloudy tiles moved: {cloud_results['cloudy_tiles']}")
-    else:
-        print("❌ Cloud detection failed or no tiles found")
-    """
+    # Step 3: Apply cloud detection function
+    print("\n--- Step 3: Applying cloud detection ---")
+    print(f"Using S2PixelCloudDetector with % of cloudy pixels threshold: {cloud_threshold}")
+    init_time = time.time()
+    cloud_results = is_cloudy(base_path, cloud_threshold)
+    dt = time.time() - init_time
+    print(f"✓ Cloud detection completed in {dt:.1f} seconds. Clean tiles: {cloud_results['clean_tiles']}, Cloudy tiles moved: {cloud_results['cloudy_tiles']}")
         
     #------------------------------------------------------------------------------------
 
-    # Step 5: Placeholder for vegetation detection model
+    # Step 4: Placeholder for vegetation detection model
     # TODO: Implement vegetation detection model that removes tiles without sufficient vegetation
     # This model should:
     # - Analyze vegetation indices (NDVI/NDMI) in each remaining patch
@@ -127,38 +102,34 @@ def full_sentinel2_data_pipeline(dataset_name: str,
 
     #------------------------------------------------------------------------------------
 
-    # Step 6: Placeholder for black pixel filtering
     # TODO: Implement filtering for black tiles (0 or smth like that for all rgb values)
-    # - check if pixel is fully black (part of image padding)
+    # - check if pixel is fully black 
     # - if so, discard it
     
     #------------------------------------------------------------------------------------
 
-    # Step 7: Extract NDVI and NDMI for remaining tiles
-    print("\n--- Step 6: Extracting NDVI and NDMI for remaining clean tiles ---")
+    # Step 5: Extract NDVI and NDMI for remaining tiles
+    print("\n--- Step 5: Extracting NDVI and NDMI for remaining clean tiles ---")
     
-    # Process remaining tiles in the tiles_BANDS folder (after cloud filtering)
+    # Process remaining tiles in the tiles folder (after cloud & vegetation filtering)
     remaining_patch_files = [f for f in os.listdir(tiles_input_path) if f.endswith('.npy')]
     
     if remaining_patch_files:
         print(f"Processing {len(remaining_patch_files)} remaining clean tiles...")
         
         # Process NDVI & NDMI tiles
-        process_tiles_directory_with_indices(tiles_input_path, band_names)
+        process_tiles_directory_with_indices(tiles_input_path, all_bands)
         print(f"✓ NDVI & NDMI values added to the tiles in {tiles_input_path}")
 
     else:
         print("⚠ No clean tiles remaining after cloud detection!")
     
-    # Step 8: Print folder locations
+    # Print folder locations
     print(f"\n=== Processing pipeline completed for dataset: {dataset_name} ===")
     print("📁 OUTPUT FOLDER LOCATIONS:")
     print(f"  🔸 Clean bands tiles +  indices (15 bands): {tiles_input_path}")
     print(f"  🔸 Label tiles (dNBR): {tiles_labels_path}")
-
-    #if cloud_results:
-    #    print(f"  🔸 Cloudy tiles (moved): {cloud_results['cloudy_dir']}")
-    print(f"  🔸 Full image results: {full_img_path}")
+    print(f"  🔸 Full image label: {full_img_path}")
 
     # Display dNBR heatmap
     ax = plt.imshow(dnbr_normmap[:, :, 0], cmap='coolwarm')
@@ -174,8 +145,8 @@ def full_sentinel2_data_pipeline(dataset_name: str,
         'bands_tiles': tiles_input_path,
         'label_tiles': tiles_labels_path,
         'full_img_results': full_img_path,
-        #'cloud_results': cloud_results
+        'cloud_results': cloud_results
     }
 
 if __name__ == '__main__':
-    full_sentinel2_data_pipeline('greece') 
+    full_sentinel2_data_pipeline('france2') 
