7ce9dbd205515128efc89ddc6957bcdba5b23e40
DitoluT <dtoledo@uma.es>
2025-08-12 15:10:54 +0300

    Add __init__.py files and remove node_modules folder

---
+++ server/utils/preprocessing/__init__.py
@@ -0,0 +1,9 @@
+"""
+Preprocessing utilities for satellite image data preparation.
+"""
+
+from .bands_preprocessing import *
+from .data_augmentation import *
+from .data_preprocessing import *
+from .geodata_extraction import *
+from .tmp_metadata_extraction import *
